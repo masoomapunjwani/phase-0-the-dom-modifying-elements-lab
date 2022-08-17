8e349727c9a28b879f53d750b2e93936351d5916
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-17 11:49:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+// Remove  DOM 'main' node
+const main = document.getElementById("main");
+main.remove();
+
+// Create newHeader variable pointing to 'h1'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Add the element as a child
+document.body.appendChild(newHeader);
+
+// Add content to the node
+newHeader.innerHTML = "Masooma is the champion";
+
+
